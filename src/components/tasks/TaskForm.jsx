5eb4eec29d3ec1d426d1848e6f7235fd063c7412
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-06-13 15:05:13 -0500

    commit 3

--- src/components/tasks/TaskForm.jsx
+++ src/components/tasks/TaskForm.jsx
@@ -25,8 +25,8 @@ function TaskForm() {
         <div className='max-w-md mx-auto'>
             <form className='bg-neutral-700 p-10 mb-4' action="" onSubmit={handleSubmit}>
                 <p className='capitalize font-bold text-lg text-white mb-3'>Crea tu Tarea</p>
-                <input className='bg-slate-300 p-3 w-full mb-2 placeholder-gray-500' placeholder='escribe tu Tarea' value={title} onChange={(e) => setTitle(e.target.value)} />
-                <textarea className='bg-slate-300 p-3 w-full mb-2 placeholder-gray-500' placeholder='Escribe la descripcion de la Tarea' value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
+                <input className='bg-slate-300 p-3 w-full mb-2 placeholder-gray-600' placeholder='escribe tu Tarea' value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea className='bg-slate-300 p-3 w-full mb-2 placeholder-gray-600' placeholder='Escribe la descripcion de la Tarea' value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
                 <button className='bg-blue-500 text-white px-3 py-1 rounded-md'>guardar</button>
                 {error ? <p className='p-3 mx-2 my-3 bg-red-500 font-bold text-white text-xl rounded-md'>hay un campo vacio</p> : ""}
             </form>
